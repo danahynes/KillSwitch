0d1c9eeb5a59572c9705ce679b9568715d7ba48f
danahynes <46582023+danahynes@users.noreply.github.com>
2019-01-27 12:50:02 -0500

    added option to turn led off completely except for programming

--- Bash/settings/killswitch-settings.sh
+++ Bash/settings/killswitch-settings.sh
@@ -8,13 +8,11 @@
 # All rights reserved.
 #-------------------------------------------------------------------------------
 
-# TODO: add option to led type for "off" (currently set LED brightness to 0)
-
 #-------------------------------------------------------------------------------
 # Constants
 
 VERSION_NUMBER="0.1"
-VERSION_BUILD="19.01.07"
+VERSION_BUILD="19.01.26"
 
 DIALOG_OK=0
 DIALOG_CANCEL=1
@@ -27,6 +25,8 @@ SETTINGS_FILE="${SETTINGS_DIR}/killswitch-settings.conf"
 SERIAL_PORT=/dev/pts/4
 SERIAL_SPEED=9600
 
+LEDO_ON=0
+LEDO_OFF=1
 LEDT_FLASH=0
 LEDT_PULSE=1
 LEDS_NORMAL=0
@@ -34,6 +34,7 @@ LEDS_INVERT=1
 LPA_REBOOT=0
 LPA_SHUTDOWN=1
 
+LEDO_DEFAULT=$LEDO_ON
 LEDB_DEFAULT=255            # 0-255
 LEDT_DEFAULT=$LEDT_FLASH
 LEDS_DEFAULT=$LEDS_NORMAL
@@ -56,9 +57,9 @@ CANCEL_LABEL="Cancel"
 
 MENU_TITLE="KillSwitch Settings"
 MENU_TEXT="Choose an item:"
-MENU_HEIGHT=15
+MENU_HEIGHT=16
 MENU_WIDTH=40
-MENU_ITEM_HEIGHT=8
+MENU_ITEM_HEIGHT=9
 MENU_TAGS=(\
     "1" \
     "2" \
@@ -69,13 +70,9 @@ MENU_TAGS=(\
     "7" \
     "8" \
     "9" \
-    "10" \
-    "11" \
 )
 MENU_ITEMS=(\
-    "LED brightness" \
-    "LED type" \
-    "LED state" \
+    "Status LED options" \
     "Start recording" \
     "Long press time" \
     "Long press action" \
@@ -86,9 +83,7 @@ MENU_ITEMS=(\
     "Exit" \
 )
 MENU_HELP=(\
-	"Set the brightness of the status LED" \
-	"Set whether the LED flashes or pulses" \
-	"Set whether the LED is on with the Pi or reversed" \
+	"Set the status LED options" \
 	"Start recording new remote codes" \
 	"Set how long to hold the button for a long press action" \
 	"Set the action to take when the button is held" \
@@ -98,6 +93,40 @@ MENU_HELP=(\
 	"Uninstall all KillSwitch files" \
 	"Quit this program" \
 )
+
+LED_MENU_TITLE="LED Options"
+LED_MENU_TEXT="Choose an item:"
+LED_MENU_HEIGHT=11
+LED_MENU_WIDTH=40
+LED_MENU_ITEM_HEIGHT=9
+LED_MENU_TAGS=(\
+    "1" \
+    "2" \
+    "3" \
+    "4" \
+)
+LED_MENU_ITEMS=(\
+    "LED on/off" \
+    "LED brightness" \
+    "LED type" \
+    "LED state" \
+)
+LED_MENU_HELP=(\
+    "Turn the LED on or off" \
+    "Set the LED brightness" \
+    "Set the led type to flash or pulse" \
+    "Set whether the is on when the pi is on, or vice versa"
+)
+
+LEDO_TITLE="LED on/off"
+LEDO_TEXT="Turn the LED on or off"
+LEDO_HEIGHT=10
+LEDO_WIDTH=40
+LEDO_ITEM_HEIGHT=2
+LEDO_TAGS=("1" "2")
+LEDO_ITEMS=("On" "Off")
+LEDO_SETTING="LEDO"
+LEDO_ACTION="LEDO"
 
 LEDB_TITLE="LED brightness"
 LEDB_TEXT="Set the LED brightness (0 - 255):"
@@ -136,10 +165,10 @@ REC_ACTION="REC"
 #REC_ON_VALUE="ON"
 REC_TITLE="Record new remote codes"
 REC_TEXT="Press OK to start recording new codes on the device. The status LED \
-will begin flashing rapidly. Point the \
-remote at the device and press the button for 'on'. When you see the status \
-LED flash once slowly, point the remote at the device and press the button \
-for 'off'. When the LED flashes slowly twice, recording is complete."
+will begin flashing rapidly. Point the remote at the device and press the \
+button for 'on'. When you see the status LED flash once slowly, point the \
+remote at the device and press the button for 'off'. When the LED flashes \
+slowly twice, recording is complete."
 #REC_OFF_VALUE="OFF"
 #REC_DONE_VALUE="DONE"
 #REC_CANCEL_VALUE="CANCEL"
@@ -169,10 +198,10 @@ LPA_ITEMS=("Reboot" "Force quit")
 LPA_SETTING="LPA"
 LPA_ACTION="LPA"
 
-FIRMWARE_TITLE="Firmware update"
-FIRMWARE_INITIAL="${HOME}/Downloads"
-FIRMWARE_HEIGHT=5
-FIRMWARE_WIDTH=40
+# FIRMWARE_TITLE="Firmware update"
+# FIRMWARE_INITIAL="${HOME}/Downloads"
+# FIRMWARE_HEIGHT=5
+# FIRMWARE_WIDTH=40
 
 SHUTDOWN_TITLE="Shutdown"
 SHUTDOWN_TEXT="Are you sure you want to shut down?"
@@ -198,8 +227,11 @@ scriptdir="${HOME}/RetroPie-Setup"
 #-------------------------------------------------------------------------------
 # Variables
 
-DONE=0
+MENU_DONE=0
 MENU_SEL=""
+LED_MENU_DONE=0
+LED_MENU_SEL=""
+LEDO_STATES=($STATE_ON $STATE_OFF)
 LEDT_STATES=($STATE_ON $STATE_OFF)
 LEDS_STATES=($STATE_ON $STATE_OFF)
 LPA_STATES=($STATE_ON $STATE_OFF)
@@ -244,41 +276,124 @@ function doMain() {
     "${MENU_TAGS[6]}" "${MENU_ITEMS[6]}" "${MENU_HELP[6]}" \
     "${MENU_TAGS[7]}" "${MENU_ITEMS[7]}" "${MENU_HELP[7]}" \
     "${MENU_TAGS[8]}" "${MENU_ITEMS[8]}" "${MENU_HELP[8]}" \
-    "${MENU_TAGS[9]}" "${MENU_ITEMS[9]}" "${MENU_HELP[9]}" \
-    "${MENU_TAGS[10]}" "${MENU_ITEMS[10]}" "${MENU_HELP[10]}" \
     3>&1 1>&2 2>&3 3>&-)
 
     BTN=$?
     if [ ! $BTN -eq $DIALOG_OK ]; then
-        DONE=1
+        MENU_DONE=1
         return
     fi
 
     MENU_SEL="$RESULT"
-
     if [ "$RESULT" = "${MENU_TAGS[0]}" ]; then
-        doLEDBrightness
+        LED_MENU_DONE=0
+        while [ $LED_MENU_DONE -eq 0 ]; do
+            doLEDMenu
+        done
     elif [ "$RESULT" = "${MENU_TAGS[1]}" ]; then
-        doLEDType
-    elif [ "$RESULT" = "${MENU_TAGS[2]}" ]; then
-        doLEDState
-    elif [ "$RESULT" = "${MENU_TAGS[3]}" ]; then
         #doRecordOn
         doStartRecording
-    elif [ "$RESULT" = "${MENU_TAGS[4]}" ]; then
+    elif [ "$RESULT" = "${MENU_TAGS[2]}" ]; then
         doLongPressTime
-    elif [ "$RESULT" = "${MENU_TAGS[5]}" ]; then
+    elif [ "$RESULT" = "${MENU_TAGS[3]}" ]; then
         doLongPressAction
-    elif [ "$RESULT" = "${MENU_TAGS[6]}" ]; then
+    elif [ "$RESULT" = "${MENU_TAGS[4]}" ]; then
         doFirmware
-    elif [ "$RESULT" = "${MENU_TAGS[7]}" ]; then
+    elif [ "$RESULT" = "${MENU_TAGS[5]}" ]; then
         doShutdown
-    elif [ "$RESULT" = "${MENU_TAGS[8]}" ]; then
+    elif [ "$RESULT" = "${MENU_TAGS[6]}" ]; then
         doReboot
-    elif [ "$RESULT" = "${MENU_TAGS[8]}" ]; then
+    elif [ "$RESULT" = "${MENU_TAGS[7]}" ]; then
     	doUninstall
-    elif [ "$RESULT" = "${MENU_TAGS[9]}" ]; then
+    elif [ "$RESULT" = "${MENU_TAGS[8]}" ]; then
         doExit
+    fi
+}
+
+function doLEDMenu () {
+    RESULT=$(dialog \
+    --backtitle "$WINDOW_TITLE" \
+    --title "$LED_MENU_TITLE" \
+    --cancel-label "$BACK_LABEL" \
+    --default-item "$LED_MENU_SEL" \
+    --item-help \
+    --menu \
+    "$LED_MENU_TEXT" \
+    $LED_MENU_HEIGHT \
+    $LED_MENU_WIDTH \
+    $LED_MENU_ITEM_HEIGHT \
+    "${LED_MENU_TAGS[0]}" "${LED_MENU_ITEMS[0]}" "${LED_MENU_HELP[0]}" \
+    "${LED_MENU_TAGS[1]}" "${LED_MENU_ITEMS[1]}" "${LED_MENU_HELP[1]}" \
+    "${LED_MENU_TAGS[2]}" "${LED_MENU_ITEMS[2]}" "${LED_MENU_HELP[2]}" \
+    "${LED_MENU_TAGS[3]}" "${LED_MENU_ITEMS[3]}" "${LED_MENU_HELP[3]}" \
+    3>&1 1>&2 2>&3 3>&-)
+
+    BTN=$?
+    if [ $BTN -eq $DIALOG_CANCEL ]; then
+        LED_MENU_DONE=1
+    elif [ $BTN -eq $DIALOG_ESCAPE ]; then
+        LED_MENU_DONE=1
+        MENU_DONE=1
+        return
+    fi
+
+    LED_MENU_SEL="$RESULT"
+    if [ "$RESULT" = "${LED_MENU_TAGS[0]}" ]; then
+        doLEDOn
+    elif [ "$RESULT" = "${LED_MENU_TAGS[1]}" ]; then
+        doLEDBrightness
+    elif [ "$RESULT" = "${LED_MENU_TAGS[2]}" ]; then
+        doLEDType
+    elif [ "$RESULT" = "${LED_MENU_TAGS[3]}" ]; then
+        doLEDState
+    fi
+}
+
+function doLEDOn() {
+    LEDO_VALUE=$(readFromFile $LEDO_SETTING)
+
+    # set highlighted item
+    LEDO_HIGHLIGHT="${LEDO_ITEMS[$LEDO_VALUE]}"
+
+    # size of array
+    SIZE=${#LEDO_STATES[@]}
+
+    # all off
+    for (( i=0; i<$SIZE; i++ )); do
+        LEDO_STATES[i]=$STATE_OFF
+    done
+
+    # set checked item
+    LEDO_STATES[$LEDO_VALUE]=$STATE_ON
+
+    RESULT=$(dialog \
+    --backtitle "$WINDOW_TITLE" \
+    --title "$LEDO_TITLE" \
+    --cancel-label "$BACK_LABEL" \
+    --default-item "$LEDO_HIGHLIGHT" \
+    --radiolist \
+    "$LEDO_TEXT" \
+    $LEDO_HEIGHT \
+    $LEDO_WIDTH \
+    $LEDO_ITEM_HEIGHT \
+    "${LEDO_TAGS[0]}" "${LEDO_ITEMS[0]}" "${LEDO_STATES[0]}" \
+    "${LEDO_TAGS[1]}" "${LEDO_ITEMS[1]}" "${LEDO_STATES[1]}" \
+    3>&1 1>&2 2>&3 3>&-)
+
+    BTN=$?
+    if [ $BTN -eq $DIALOG_OK ]; then
+
+        # save highlighted/selected item
+        for (( i=0; i<$SIZE; i++ )); do
+            if [ "${LEDO_TAGS[i]}" = "$RESULT" ]; then
+                writeToFile $LEDO_SETTING $i
+                sendSerial $LEDO_ACTION $i
+                break
+            fi
+        done
+    elif [ $BTN -eq $DIALOG_ESCAPE ]; then
+        LED_MENU_DONE=1
+        MENU_DONE=1
     fi
 }
 
@@ -306,7 +421,8 @@ function doLEDBrightness() {
         writeToFile $LEDB_SETTING $RESULT
         sendSerial $LEDB_ACTION $RESULT
     elif [ $BTN -eq $DIALOG_ESCAPE ]; then
-        DONE=1
+        LED_MENU_DONE=1
+        MENU_DONE=1
     fi
 }
 
@@ -353,7 +469,8 @@ function doLEDType() {
             fi
         done
     elif [ $BTN -eq $DIALOG_ESCAPE ]; then
-        DONE=1
+        LED_MENU_DONE=1
+        MENU_DONE=1
     fi
 }
 
@@ -400,7 +517,8 @@ function doLEDState() {
             fi
         done
     elif [ $BTN == $DIALOG_ESCAPE ]; then
-        DONE=1
+        LED_MENU_DONE=1
+        MENU_DONE=1
     fi
 }
 
@@ -443,7 +561,7 @@ function doLEDState() {
 #
 #        # put arduino in rec cancel mode
 #        sendSerial $REC_ACTION $REC_CANCEL_VALUE
-#        DONE=1
+#        MENU_DONE=1
 #    fi
 #}
 
@@ -477,7 +595,7 @@ function doLEDState() {
 #
 #        # put arduino in rec cancel mode
 #        sendSerial $REC_ACTION $REC_CANCEL_VALUE
-#        DONE=1
+#        MENU_DONE=1
 #    fi
 #}
 
@@ -497,7 +615,7 @@ function doStartRecording() {
     if [ $BTN -eq $DIALOG_OK ]; then
         sendSerial $REC_ACTION ""
     elif [ $BTN -eq $DIALOG_ESCAPE ]; then
-        DONE=1
+        MENU_DONE=1
     fi
 }
 
@@ -525,7 +643,7 @@ function doLongPressTime() {
         writeToFile $LPT_SETTING $RESULT
         sendSerial $LPT_ACTION $RESULT
     elif [ $BTN -eq $DIALOG_ESCAPE ]; then
-        DONE=1
+        MENU_DONE=1
     fi
 }
 
@@ -572,13 +690,15 @@ function doLongPressAction() {
             fi
         done
     elif [ $BTN -eq $DIALOG_ESCAPE ]; then
-        DONE=1
+        MENU_DONE=1
     fi
 }
 
 # TODO: this is ugly and bad. the dialog only works with a keyboard, because you
 # need the spacebar to accept a selected dir/file and backslash to get it to
 # move to the selected dir.
+#
+# github token: 3868839158c75239f3ed89a4aedfe620e72156b4
 #
 # what needs to happen:
 # 1. check for internet or wget firmware version file
@@ -587,19 +707,30 @@ function doLongPressAction() {
 # 4. scan for downloaded version in ${HOME}/.killswitch
 # 5. show message 'do you want to update'
 # 6. run avrdude with path to full firmware hex file
+# function doFirmware() {
+#     # TODO firmware result
+#     # stdout is bad, but only way this works
+#     RESULT=$(dialog \
+#     --stdout \
+#     --backtitle "$WINDOW_TITLE" \
+#     --title "$FIRMWARE_TITLE" \
+#     --cancel-label "$BACK_LABEL" \
+#     --fselect \
+#     "$FIRMWARE_INITIAL" \
+#     $FIRMWARE_HEIGHT \
+#     $FIRMWARE_WIDTH \
+#     3>&1 1>&2 2>&3 3>&-)
+# }
+
 function doFirmware() {
-    # TODO firmware result
-    # stdout is bad, but only way this works
-    RESULT=$(dialog \
-    --stdout \
-    --backtitle "$WINDOW_TITLE" \
-    --title "$FIRMWARE_TITLE" \
-    --cancel-label "$BACK_LABEL" \
-    --fselect \
-    "$FIRMWARE_INITIAL" \
-    $FIRMWARE_HEIGHT \
-    $FIRMWARE_WIDTH \
-    3>&1 1>&2 2>&3 3>&-)
+    curl \
+    -H 'Authorization: token 3868839158c75239f3ed89a4aedfe620e72156b4' \
+    -H 'Accept: application/vnd.github.v3.raw' \
+    -o "${SETTINGS_DIR}" \
+    -L https://api.github.com/repos/danahynes/KillSwitch/contents/Arduino/version.txt
+
+    sendSerial "VER" ""
+    CURRENT_VERSION=readSerial
 }
 
 function doShutdown() {
@@ -614,10 +745,11 @@ function doShutdown() {
 
     BTN=$?
     if [ $BTN -eq $DIALOG_OK ]; then
+        # TODO: replace this with real call
         echo "$SHUTDOWN_COMMAND"
-        DONE=1
+        MENU_DONE=1
     elif [ $BTN -eq $DIALOG_ESCAPE ]; then
-        DONE=1
+        MENU_DONE=1
     fi
 }
 
@@ -633,10 +765,11 @@ function doReboot() {
 
     BTN=$?
     if [ $BTN -eq $DIALOG_OK ]; then
+        # TODO: replace this with real call
         echo "$REBOOT_COMMAND"
-        DONE=1
+        MENU_DONE=1
     elif [ $BTN -eq $DIALOG_ESCAPE ]; then
-        DONE=1
+        MENU_DONE=1
     fi
 }
 
@@ -652,15 +785,16 @@ function doUninstall() {
 
     BTN=$?
     if [ $BTN -eq $DIALOG_OK ]; then
+        # TODO: replace this with real call
         echo "$UNINSTALL_COMMAND"
-        DONE=1
+        MENU_DONE=1
     elif [ $BTN -eq $DIALOG_ESCAPE ]; then
-        DONE=1
+        MENU_DONE=1
     fi
 }
 
 function doExit() {
-    DONE=1
+    MENU_DONE=1
 }
 
 #function doReadSerial() {
@@ -678,6 +812,7 @@ if [ ! -f "$SETTINGS_FILE" ]; then
     # if not, write defaults
     mkdir -p "${SETTINGS_DIR}"
     touch "$SETTINGS_FILE"
+    echo $LEDO_SETTING"="$LEDO_DEFAULT >> "$SETTINGS_FILE"
     echo $LEDB_SETTING"="$LEDB_DEFAULT >> "$SETTINGS_FILE"
     echo $LEDT_SETTING"="$LEDT_DEFAULT >> "$SETTINGS_FILE"
     echo $LEDS_SETTING"="$LEDS_DEFAULT >> "$SETTINGS_FILE"
@@ -697,7 +832,7 @@ stty -F $SERIAL_PORT speed $SERIAL_SPEED -cstopb -parenb cs8
 #-------------------------------------------------------------------------------
 # Main loop
 
-while [ $DONE -eq 0 ]; do
+while [ $MENU_DONE -eq 0 ]; do
 #    doReadSerial
     doMain
 done
