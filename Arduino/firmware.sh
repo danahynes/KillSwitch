dc387439b1c5ae1b4c81ec1ad0a0dc54bdc459d2
danahynes <46582023+danahynes@users.noreply.github.com>
2019-01-28 08:11:04 -0500

    changed output file name

--- Arduino/firmware.sh
+++ Arduino/firmware.sh
@@ -1,4 +1,4 @@
 VERSION_NUMBER=$(grep "VERSION_NUMBER=" "version.txt" | cut -d "=" -f2)
 VERSION_BUILD=$(grep "VERSION_BUILD=" "version.txt" | cut -d "=" -f2)
 cd .pioenvs/uno
-cp firmware.hex "../../firmware-${VERSION_NUMBER}-${VERSION_BUILD}.hex"
+cp firmware.hex "../../killswitch-firmware-${VERSION_NUMBER}-${VERSION_BUILD}.hex"
