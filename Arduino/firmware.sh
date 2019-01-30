59e0d2374a24e77da4c90b374461babceec79975
danahynes <46582023+danahynes@users.noreply.github.com>
2019-01-29 19:39:31 -0500

    moved firmware.sh

--- Arduino/firmware.sh
+++
@@ -1,4 +0,0 @@
-VERSION_NUMBER=$(grep "VERSION_NUMBER=" "version.txt" | cut -d "=" -f2)
-VERSION_BUILD=$(grep "VERSION_BUILD=" "version.txt" | cut -d "=" -f2)
-cd .pioenvs/uno
-cp firmware.hex "../../killswitch-firmware-${VERSION_NUMBER}-${VERSION_BUILD}.hex"
