da090bfa20b8457ac392103eeb7f88139aca7a7f
danahynes <46582023+danahynes@users.noreply.github.com>
2019-01-30 13:50:50 -0500

    new package

--- Bash/install/killswitch-uninstall.sh
+++ Bash/install/killswitch-uninstall.sh
@@ -17,7 +17,7 @@ VERSION_BUILD="19.01.30"
 #-------------------------------------------------------------------------------
 # variables
 
-SETTINGS_DIR="${HOME}/.killswitch"
+SETTINGS_DIR="/home/${SUOD_USER}/.killswitch"
 echo $SETTINGS_DIR
 SETTINGS_FILE="${SETTINGS_DIR}/killswitch-settings.conf"
 
