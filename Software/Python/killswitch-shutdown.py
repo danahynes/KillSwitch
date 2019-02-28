db55d1ce66d75ecade692e6cd1fe48c841516512
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2019-02-28 10:10:30 -0500

    updated version number and removed serial code

--- Software/Python/killswitch-shutdown.py
+++ Software/Python/killswitch-shutdown.py
@@ -8,7 +8,7 @@
 # All rights reserved.
 #-------------------------------------------------------------------------------
 
-VERSION_NUMBER = "0.3.3"
+VERSION_NUMBER = "0.3.4"
 
 # are we running on a pi?
 onPi = False
@@ -21,7 +21,6 @@ try :
     onPi = True
 except ImportError:
     onPi = False
-#import serial
 import sys
 import time
 
@@ -29,12 +28,6 @@ import time
 # constants
 #-------------------------------------------------------------------------------
 pin_feedback = 2
-
-#if (onPi):
-#    serial_port = '/dev/ttyS0'
-#else:
-#    serial_port = '/dev/pts/5'
-#serial_speed = 9600
 
 #-------------------------------------------------------------------------------
 # objects
@@ -46,9 +39,6 @@ if (onPi):
     # when the script starts, this pin will stay LOW
     # we then pulse the pin based on whether we are shutting down or rebooting
     feedback = g0.OutputDevice(pin_feedback)
-
-# set up serial
-#ser = serial.Serial(serial_port, serial_speed, timeout = 1);
 
 #-------------------------------------------------------------------------------
 # main code
@@ -63,8 +53,6 @@ if (sys.argv[1] == "poweroff") or (sys.argv[1] == "halt"):
         time.sleep(0.02)
         feedback.off()
 
-    # write to serial
-    #ser.write("?SHT|!")
 elif (sys.argv[1] == "reboot"):
 
     if (onPi):
@@ -77,8 +65,5 @@ elif (sys.argv[1] == "reboot"):
         feedback.on()
         time.sleep(0.02)
         feedback.off()
-
-    # write to serial
-    #ser.write("?RBT|!")
 
 # -)
