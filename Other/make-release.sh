40094f36ca98625e4a47c165bc9c5daa745cca96
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2019-03-04 20:28:25 -0500

    change version number

--- Other/make-release.sh
+++ Other/make-release.sh
@@ -10,7 +10,7 @@
 # by Sam Hocevar. See the LICENSE file for more details.
 #-------------------------------------------------------------------------------
 
-VERSION_NUMBER="0.3.4"
+VERSION_NUMBER="0.4.0"
 
 # change firmware
 cd ../Firmware/
