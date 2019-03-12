250d2b65c14be2cb3c56c57f61fd720d4f0d592d
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2019-03-11 23:26:57 -0400

    version bump

--- Software/Python/killswitch-boot.py
+++ Software/Python/killswitch-boot.py
@@ -10,7 +10,7 @@
 # by Sam Hocevar. See the LICENSE file for more details.
 #-------------------------------------------------------------------------------
 
-VERSION_NUMBER = "0.4.1"
+VERSION_NUMBER = "0.4.2"
 
 DEBUG = 1
 
