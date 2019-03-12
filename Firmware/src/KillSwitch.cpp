7dc016a85b60d7dd64753fe6dfae8fed8a99a2ca
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2019-03-11 23:24:19 -0400

    version bump

--- Firmware/src/KillSwitch.cpp
+++ Firmware/src/KillSwitch.cpp
@@ -28,7 +28,7 @@
 //-----------------------------------------------------------------------------
 // Constants
 
-const char VERSION_NUMBER[] PROGMEM = "0.4.1";
+const char VERSION_NUMBER[] PROGMEM = "0.4.2";
 
 const int STATE_OFF = 0;
 const int STATE_BOOTING = 1;
