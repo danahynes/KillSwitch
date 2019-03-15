43b1d9983c7a6a98b491eb338303a467484b84b5
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2019-03-15 17:37:13 -0400

    version bump

--- Other/make-release.sh
+++ Other/make-release.sh
@@ -10,7 +10,7 @@
 # by Sam Hocevar. See the LICENSE file for more details.
 #-------------------------------------------------------------------------------
 
-VERSION_NUMBER="0.4.3"
+VERSION_NUMBER="0.4.4"
 
 # change firmware
 cd ../Firmware/
