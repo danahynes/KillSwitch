5a6915092e8c8fc8b871db6be5fbb822acf066d5
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2019-03-15 22:29:25 -0400

    version bump

--- Firmware/src/KillSwitch.cpp
+++ Firmware/src/KillSwitch.cpp
@@ -28,7 +28,7 @@
 //-----------------------------------------------------------------------------
 // Constants
 
-const char VERSION_NUMBER[] PROGMEM = "0.4.4";
+const char VERSION_NUMBER[] PROGMEM = "0.4.5";
 
 const int STATE_OFF = 0;
 const int STATE_BOOTING = 1;
--- Software/Python/killswitch-settings.py
+++ Software/Python/killswitch-settings.py
@@ -27,7 +27,7 @@ import zipfile
 #-------------------------------------------------------------------------------
 # Constants
 
-VERSION_NUMBER = "0.4.4"
+VERSION_NUMBER = "0.4.5"
 
 DEBUG = os.uname()[4].startswith("arm")
 
