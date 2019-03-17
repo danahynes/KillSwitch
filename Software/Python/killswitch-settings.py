85b0322b4bee78e8e562999f46765f24def9448c
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2019-03-17 19:31:51 -0400

    version bump

--- Software/Python/killswitch-settings.py
+++ Software/Python/killswitch-settings.py
@@ -27,7 +27,7 @@ import zipfile
 #-------------------------------------------------------------------------------
 # Constants
 
-VERSION_NUMBER = "0.4.14"
+VERSION_NUMBER = "0.4.15"
 
 DEBUG = os.uname()[4].startswith("arm")
 
