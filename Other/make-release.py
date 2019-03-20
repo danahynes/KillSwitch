bb2c07c1d84f0688b128742354f6924845f1785d
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2019-03-20 19:33:19 -0400

    move kicad logos to correct folders

---
+++ Other/make-release.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+
+#-------------------------------------------------------------------------------
+# make-release.py
+# KillSwitch
+#
+# Copyright © 2019 Dana Hynes <danahynes@gmail.com>
+# This work is free. You can redistribute it and/or modify it under the terms
+# of the Do What The Fuck You Want To Public License, Version 2, as published
+# by Sam Hocevar. See the LICENSE file for more details.
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# Imports
+
+
+#-------------------------------------------------------------------------------
+# Constants
+VERSION_NUMBER = "0.5.2"
+
+# change install-latest
+os.chdir("..")
+
+FILE = "install-latest.py"
