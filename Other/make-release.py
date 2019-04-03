d72d8d0ac9d1dc6d34fe97289091a6ca3e33a8eb
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2019-04-02 22:32:36 -0400

    version bump

--- Other/make-release.py
+++ Other/make-release.py
@@ -25,7 +25,7 @@ import time
 #-------------------------------------------------------------------------------
 # Constants
 
-VERSION_NUMBER = "0.5.6"
+VERSION_NUMBER = "0.5.7"
 
 # set locale
 locale.setlocale(locale.LC_ALL, '')
--- Software/Python/killswitch-settings.py
+++ Software/Python/killswitch-settings.py
@@ -29,7 +29,7 @@ import zipfile
 #-------------------------------------------------------------------------------
 # Constants
 
-VERSION_NUMBER = "0.5.6"
+VERSION_NUMBER = "0.5.7"
 
 DEBUG = os.uname()[4].startswith("arm")
 
